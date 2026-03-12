5d8047ea98ad5246a0bb609728fcb5b0c94e262a
karthickraji <karthickcool17@gmail.com>
2026-03-12 17:41:43 +0530

    5th commit - added SparkSubmitOperator to airflow

--- airflow/dags/sales_data_spark_pipeline_dag.py
+++ airflow/dags/sales_data_spark_pipeline_dag.py
@@ -1,26 +1,69 @@
 from airflow import DAG
-from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
-# Define default arguments for the DAG
+PROJECT_PATH = "/home/karthick/PycharmProjects/sales_data_pipeline_using_spark"
+
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "data_engineer",
+    "retries": 2,
+    "retry_delay": timedelta(minutes=2),
 }
 
+def processed_data_quality_check():
+    import os
+    path = "/project_one_data/processed/sales"
+    if not os.path.exists(path):
+        raise ValueError("Processed data not found!")
+
+def staging_data_quality_check():
+    import os
+    path = "/project_one_data/staging/sales"
+    if not os.path.exists(path):
+        raise ValueError("Staging data not found!")
+
 with DAG(
-    dag_id="sales_data_spark_pipeline",
-    start_date=datetime(2026, 3, 11),
+    dag_id="spark_jobs_etl_sales_data_pipeline",
+    start_date=datetime(2026, 3, 12),
     schedule=timedelta(minutes=5),
-    # schedule="@daily",
-    catchup=False
+    catchup=False,
+    max_active_runs=1,
+    default_args=default_args
 ) as dag:
 
-    task1 = BashOperator(
-        task_id="run_etl",
-        bash_command="cd /home/karthick/PycharmProjects/sales_data_pipeline_using_spark && bash scripts/run_pipeline.sh;"
+    extract = SparkSubmitOperator(
+        task_id="extract_data",
+        application=f"{PROJECT_PATH}/spark_jobs/extract.py",
+        conn_id="spark_default"
     )
+
+    transform = SparkSubmitOperator(
+        task_id="transform_data",
+        application=f"{PROJECT_PATH}/spark_jobs/transform.py",
+        conn_id="spark_default"
+    )
+
+    load_hive = SparkSubmitOperator(
+        task_id="load_hive",
+        application=f"{PROJECT_PATH}/spark_jobs/load_hive.py",
+        conn_id="spark_default"
+    )
+
+    processed_data_qty_check = PythonOperator(
+        task_id="processed_data_quality_check",
+        python_callable=processed_data_quality_check
+    )
+
+    staging_data_qty_check = PythonOperator(
+        task_id="staging_data_quality_check",
+        python_callable=staging_data_quality_check
+    )
+
+    aggregate = SparkSubmitOperator(
+        task_id="aggregate_data",
+        application=f"{PROJECT_PATH}/spark_jobs/load_to_mysql.py",
+        conn_id="spark_default"
+    )
+
+    extract >> staging_data_qty_check >> transform >> processed_data_qty_check >> load_hive >> aggregate
--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,4 +1,18 @@
-python -m spark_jobs.extract
+#!/bin/bash
+
+echo "Starting Sales Spark ETL Pipeline"
+
+# go to project root
+# shellcheck disable=SC2164
+cd /home/karthick/PycharmProjects/sales_data_pipeline_using_spark
+
+# activate virtual environment (optional)
+source venv/bin/activate
+
+# run pipeline
+python -m spark_jobs.extractsh
 python -m spark_jobs.transform
 python -m spark_jobs.load_hive
 python -m mysql_scripts.load_to_mysql
+
+echo "Pipeline Finished"
